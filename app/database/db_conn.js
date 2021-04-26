87513ddc249a7e16ee9a616502a2cc15a7369a4a
vin-albertus <vinalbertus@gmail.com>
2021-04-26 14:44:55 +0700

    fixing connection

--- app/database/db_conn.js
+++ app/database/db_conn.js
@@ -2,17 +2,11 @@ const mysql = require("mysql");
 const dbConfig = require("../config/db.config.js");
 
 // Create a connection to the database
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   database: dbConfig.DB,
-});
-
-// open the MySQL connection
-db.connect((error) => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
 });
 
 module.exports = db;
--- app/server.js
+++ app/server.js
@@ -11,66 +11,42 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Connected to server! See list of service that provided.",
-    list_api: `
-      ======================================
-      Api for Movie
-      ======================================
-      post("/api/movie")
-      get("/api/movie")
-      get("/api/movie/:movie_id")
-      put("/api/movie/:movie_id")
-    
-      ======================================
-      Api for Genre
-      ======================================
-      get("/api/genre")
-      post("/api/genre")
-    
-      ======================================
-      Api for Comment
-      ======================================
-      post("/api/comment/:movie_id")
-      get("/api/comment/:movie_id")
-    
-      ======================================
-      Api for Rating
-      ======================================
-      post("/api/rating/:movie_id")
-      put("/api/rating/:movie_id")
-    
-      ======================================
-      Api for Watched
-      ======================================
-      get("/api/watched/:token")
-      post("/api/watched/:movie_id")
-      delete("/api/watched/:movie_id")
-    
-      ======================================
-      Api for Watchlist
-      ======================================
-      get("/api/watchlist/:token")
-      post("/api/watchlist/:movie_id")
-      delete("/api/watchlist/:movie_id")
-    
-      ======================================
-      Api for Cast (done with req token)
-      ======================================
-      post("/api/cast")
-      get("/api/cast")
-      get("/api/cast/:id")
-      put("/api/cast/:id")
-    
-      ======================================
-      Api for User (done with req token)
-      ======================================
-      get("/api/user")
-      get("/api/user/:token")
-      post("/api/login")
-      post("/api/logout")
-      post("/api/register")
-      put("/api/user")
-      get("/api/areyoulogin/:username")
-      `,
+    list_api: {
+      movie: [
+        "post: /api/movie",
+        "get: /api/movie",
+        "get: /api/movie/:movie_id",
+        "put: /api/movie/:movie_id",
+      ],
+      genre: ["get: /api/genre", "post: /api/genre"],
+      comment: ["post: /api/comment/:movie_id", "get: /api/comment/:movie_id"],
+      rating: ["post: /api/rating/:movie_id", "put: /api/rating/:movie_id"],
+      watched: [
+        "get: /api/watched/:token",
+        "post: /api/watched/:movie_id",
+        "delete: /api/watched/:movie_id",
+      ],
+      watchlist: [
+        "get: /api/watchlist/:token",
+        "post: /api/watchlist/:movie_id",
+        "delete: /api/watchlist/:movie_id",
+      ],
+      cast: [
+        "post: /api/cast",
+        "get: /api/cast",
+        "get: /api/cast/:id",
+        "put: /api/cast/:id",
+      ],
+      user: [
+        "get: /api/user",
+        "get: /api/user/:token",
+        "post: /api/login",
+        "post: /api/logout",
+        "post: /api/register",
+        "put: /api/user",
+        "get: /api/areyoulogin/:username",
+      ],
+    },
   });
 });
 
