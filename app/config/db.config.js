5942df273ce4b364c92d46503af81665baa68c06
vin-albertus <vinalbertus@gmail.com>
2021-04-26 14:18:32 +0700

    change config db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "movie_api",
+  HOST: "us-cdbr-east-03.cleardb.com",
+  USER: "bc6738f00a3cb2",
+  PASSWORD: "1b8d563e",
+  DB: "heroku_39a17125e70bcac",
 };
