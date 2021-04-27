1dd2b1af9eb0b6a883561c54e6dce77425282b73
vin-albertus <vinalbertus@gmail.com>
2021-04-28 00:04:55 +0700

    change config db from local to heroku

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,13 @@
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "movie_api",
-};
-
 // module.exports = {
-//   HOST: "us-cdbr-east-03.cleardb.com",
-//   USER: "bc6738f00a3cb2",
-//   PASSWORD: "1b8d563e",
-//   DB: "heroku_39a17125e70bcac",
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB: "movie_api",
 // };
+
+module.exports = {
+  HOST: "us-cdbr-east-03.cleardb.com",
+  USER: "bc6738f00a3cb2",
+  PASSWORD: "1b8d563e",
+  DB: "heroku_39a17125e70bcac",
+};
